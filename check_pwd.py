601b2c9b794ab36b8a0d4f2c3eeb72116310b64a
dongyisun <56062169+dongyisun@users.noreply.github.com>
2020-07-27 20:20:44 -0700

    Update check_pwd.py

--- check_pwd.py
+++ check_pwd.py
@@ -1,15 +1,14 @@
-import re
-
-
 def check_pwd(pwd):
+    accepted_symbols = ["~", "`", "!", "@", "#", "$", "%", "^",
+                        "&", "*", "(", ")", "_", "+", "-", "="]
     if len(pwd) < 8 or len(pwd) > 20:
         return False
-    elif not re.search("[a-z]", pwd):
+    elif not any(map(str.islower, pwd)):
         return False
-    elif not re.search("[A-Z]", pwd):
+    elif not any(map(str.isupper, pwd)):
         return False
-    elif not re.search("[0-9]", pwd):
+    elif not any(map(str.isdigit, pwd)):
         return False
-    elif not re.search("~`!@#$%^&*()_+-=", pwd):
+    elif not any(symbol in pwd for symbol in accepted_symbols):
         return False
     return True
