461230e74357aa66450ff0a939bf43a3ef910c26
dongyisun <56062169+dongyisun@users.noreply.github.com>
2020-07-27 20:27:54 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,19 +6,19 @@ from check_pwd import check_pwd
 class TestCase(TestCase):
 
     def testLengthRequirement(self):
-        self.assertFalse(check_pwd("aB1#"))
+        self.assertFalse(check_pwd("cD24#"),)
 
     def test_Lower_case(self):
-        self.assertFalse(check_pwd("AB%2CD(3"))
+        self.assertFalse(check_pwd("CD%2AC(5"))
 
     def test_Upper_case(self):
-        self.assertFalse(check_pwd("ab@4cd$5ef)6gh^7"))
+        self.assertFalse(check_pwd("cd@5abv$ef)qh^2"))
 
     def test_Digit(self):
-        self.assertFalse(check_pwd("iEI~jFJ`kGK&lHL_"))
+        self.assertFalse(check_pwd("Cdsf~jFJ`szK&2sL_"))
 
     def test_Special_Character(self):
-        self.assertFalse(check_pwd("Mm8qNn9QOo0rPp1R"))
+        self.assertFalse(check_pwd("nN825Qn9QQo2rQp1H"))
         
 
 if __name__ == '__main__':
